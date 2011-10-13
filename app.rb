fca0099a2b07fd1dbecc650fc940065ad6368e22
Endel Dreyer <endel.dreyer@gmail.com>
2011-10-13 14:25:36 -0300

    Use Rack::Cache for cache-control

--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
 require 'sinatra'
+require "rack/cache"
 require 'socialoud'
 require 'erb'
 
+use Rack::Cache
+
 get '/' do
+  cache_control :public, :max_age => 36000
   erb :index, :locals => {
     :social => Socialoud::Client.configure(File.expand_path('config/socialoud.yml'))
   }
--- config.ru
+++ config.ru
@@ -1,6 +1,8 @@
 require 'sinatra'
+require "rack/cache"
 
 set :public_folder, File.dirname(__FILE__) + '/public'
+use Rack::Cache
 
 require './app'
 run Sinatra::Application
